f5a969996354253691801e300b71ec666cfea9d0
Helga <helgasencukova@gmail.com>
2022-11-29 22:00:02 +0300

    syntax fixed in 2 and 5 ex

--- Example005_Conditionifelse/Program.cs
+++ Example005_Conditionifelse/Program.cs
@@ -1,6 +1,8 @@
 ﻿Console.WriteLine("Введите имя пользоавтеля: ");
-string username = Console.ReadLine();
+string? username = Console.ReadLine();
 
+if(username!=null)
+{
 if(username.ToLower() == "lelinka")
 {
     Console.WriteLine("Привет, детка! Ты круче всех! ;)");
@@ -9,4 +11,5 @@ else
 {
     Console.Write("Привет, ");
     Console.WriteLine(username);
+}
 }
