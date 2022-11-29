0b7b5ceb8a997e4ee66d20974880bea3899f3688
Helga <helgasencukova@gmail.com>
2022-11-29 13:39:17 +0300

    Example005_Conditionifels added

---
+++ Example005_Conditionifelse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользоавтеля: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "lelinka")
+{
+    Console.WriteLine("Привет, детка! Ты круче всех! ;)");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
